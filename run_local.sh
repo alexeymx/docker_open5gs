86693721ce8e6e1dc063abbad9bf024592b311af
alexey <alexey@vinoc.mx>
2025-03-07 15:56:33 +0100

    files

---
+++ run_local.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -a
+source .env
+set +a
+sudo ufw disable
+sudo sysctl -w net.ipv4.ip_forward=1
+sudo cpupower frequency-set -g performance
+
+# For 4G deployment only
+docker-compose -f 4g-volte-opensips-ims-deploy.yaml build
