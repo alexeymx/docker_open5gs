4fea70b825c0ebca2f14fa8b5df877cae0d0df7c
ilinsky <alexey@vinoc.mx>
2025-03-17 00:04:30 -0600

    test

--- build.sh
+++ build.sh
@@ -11,15 +11,3 @@ docker build --no-cache --force-rm -t docker_open5gs .
 # Build docker images for kamailio IMS components
 cd ../ims_base
 docker build --no-cache --force-rm -t docker_kamailio .
-
-# Build docker images for srsRAN_4G eNB + srsUE (4G+5G)
-cd ../srslte
-docker build --no-cache --force-rm -t docker_srslte .
-
-# Build docker images for srsRAN_Project gNB
-cd ../srsran
-docker build --no-cache --force-rm -t docker_srsran .
-
-# Build docker images for UERANSIM (gNB + UE)
-cd ../ueransim
-docker build --no-cache --force-rm -t docker_ueransim .
