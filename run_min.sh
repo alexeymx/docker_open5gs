1647714688097157251f146c618b31fce5f969ac
ilinsky <alexey@vinoc.mx>
2025-03-17 20:25:13 -0600

    test

--- run_min.sh
+++ run_min.sh
@@ -9,5 +9,5 @@ sudo sysctl -w net.ipv4.ip_forward=1
 sudo cpupower frequency-set -g performance
 
 # For 4G deployment only
-docker-compose -f 4g-volte-min.yaml build
-docker-compose -f 4g-volte-min.yaml up -d
+docker compose -f 4g-volte-min.yaml build
+docker compose -f 4g-volte-min.yaml up -d
