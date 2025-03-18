3581b0f4bbba1a0ba3b566d2f8753b8b35d4c8f2
ilinsky <alexey@vinoc.mx>
2025-03-17 19:51:37 -0600

    test

---
+++ run_min.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git pull
+set -a
+source .env-min
+set +a
+sudo ufw disable
+sudo sysctl -w net.ipv4.ip_forward=1
+sudo cpupower frequency-set -g performance
+
+# For 4G deployment only
+docker-compose -f 4g-volte-min.yaml build
+docker-compose -f 4g-volte-min.yaml up -d
