0de5be15fdf542944ce91f3766f06a57098bf65b
ilinsky <alexey@vinoc.mx>
2025-03-28 01:23:12 -0600

    Add default test configuration file for CGRateS
    Introduced a detailed test configuration file (testconfg.json) for CGRateS, including settings for general system configs, data databases, store databases, listeners, and caching. This provides a comprehensive reference for default values in testing setups.

---
+++ recreate_pyhss.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git pull
+set -a
+source .env-min
+set +a
+
+# For 4G deployment only
+docker-compose -f 4g-volte-min.yaml build --no-cache pyhss
+docker-compose -f 4g-volte-min.yaml rm -f pyhss
+docker-compose -f 4g-volte-min.yaml up -d --force-recreate pyhss
