c3f521c32cd04476cc0e4ca701dcb791377da436
ilinsky <alexey@vinoc.mx>
2025-04-20 04:31:04 -0600

    Add Metering-Method AVP to Charging Rule Definition
    Included the Metering-Method AVP (Attribute-Value Pair) in the Charging Rule Definition for enhanced functionality. Updated logic ensures proper construction of the ChargingRuleDef with the additional parameter.

--- mme/mme_init.sh
+++ mme/mme_init.sh
@@ -31,29 +31,29 @@ export IF_NAME=$(ip r | awk '/default/ { print $5 }')
 
 [ ${#MNC} == 3 ] && EPC_DOMAIN="epc.mnc${MNC}.mcc${MCC}.3gppnetwork.org" || EPC_DOMAIN="epc.mnc0${MNC}.mcc${MCC}.3gppnetwork.org"
 
-cp /mnt/mme/mme.yaml /etc/open5gs
-cp /mnt/mme/mme.conf /etc/freeDiameter
-cp /mnt/mme/make_certs.sh /etc/freeDiameter
+#cp /mnt/mme/mme.yaml /etc/open5gs
+#cp /mnt/mme/mme.conf /etc/freeDiameter
+#cp /mnt/mme/make_certs.sh /etc/freeDiameter
 
-sed -i 's|MNC|'$MNC'|g' install/etc/open5gs/mme.yaml
-sed -i 's|MCC|'$MCC'|g' install/etc/open5gs/mme.yaml
-sed -i 's|MME_IP|'$MME_IP'|g' install/etc/open5gs/mme.yaml
-sed -i 's|MME_IF|'$IF_NAME'|g' install/etc/open5gs/mme.yaml
-sed -i 's|OSMOMSC_IP|'$OSMOMSC_IP'|g' install/etc/open5gs/mme.yaml
-sed -i 's|SGWC_IP|'$SGWC_IP'|g' install/etc/open5gs/mme.yaml
-sed -i 's|SMF_IP|'$SMF_IP'|g' install/etc/open5gs/mme.yaml
-sed -i 's|MAX_NUM_UE|'$MAX_NUM_UE'|g' install/etc/open5gs/mme.yaml
-sed -i 's|MME_IP|'$MME_IP'|g' install/etc/freeDiameter/mme.conf
-sed -i 's|HSS_IP|'$HSS_IP'|g' install/etc/freeDiameter/mme.conf
+sed -i 's|MNC|'$MNC'|g' /etc/open5gs/mme.yaml
+sed -i 's|MCC|'$MCC'|g' /etc/open5gs/mme.yaml
+sed -i 's|MME_IP|'$MME_IP'|g' /etc/open5gs/mme.yaml
+sed -i 's|MME_IF|'$IF_NAME'|g' /etc/open5gs/mme.yaml
+sed -i 's|OSMOMSC_IP|'$OSMOMSC_IP'|g' /etc/open5gs/mme.yaml
+sed -i 's|SGWC_IP|'$SGWC_IP'|g' /etc/open5gs/mme.yaml
+sed -i 's|SMF_IP|'$SMF_IP'|g' /etc/open5gs/mme.yaml
+sed -i 's|MAX_NUM_UE|'$MAX_NUM_UE'|g' /etc/open5gs/mme.yaml
+sed -i 's|MME_IP|'$MME_IP'|g' /etc/freeDiameter/mme.conf
+sed -i 's|HSS_IP|'$HSS_IP'|g' /etc/freeDiameter/mme.conf
 
-sed -i 's|SGS_IP|'$SGS_IP'|g' install/etc/open5gs/mme.yaml
-
-
+sed -i 's|SGS_IP|'$SGS_IP'|g' /etc/open5gs/mme.yaml
 
 
-sed -i 's|EPC_DOMAIN|'$EPC_DOMAIN'|g' install/etc/freeDiameter/mme.conf
-sed -i 's|LD_LIBRARY_PATH|'$LD_LIBRARY_PATH'|g' install/etc/freeDiameter/mme.conf
-sed -i 's|EPC_DOMAIN|'$EPC_DOMAIN'|g' install/etc/freeDiameter/make_certs.sh
+
+
+sed -i 's|EPC_DOMAIN|'$EPC_DOMAIN'|g' /etc/freeDiameter/mme.conf
+sed -i 's|LD_LIBRARY_PATH|'$LD_LIBRARY_PATH'|g' /etc/freeDiameter/mme.conf
+sed -i 's|EPC_DOMAIN|'$EPC_DOMAIN'|g' /etc/freeDiameter/make_certs.sh
 
 
 # Generate TLS certificates
